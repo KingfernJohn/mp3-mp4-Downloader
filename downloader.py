86304efbfd7f01f5064334f92b11eabbaea6cbda
KingfernServ <49335210+KingfernJohn@users.noreply.github.com>
2022-06-10 00:00:00 +0200

    Add files via upload

---
+++ downloader.py
@@ -0,0 +1,16 @@
+import os
+print("\nDOWNLOADER\n")
+def spotify_download():
+    pass
+
+def youtube_download():
+    print("\n1 - mp3")
+    print("2 - mp4\n")
+    youtube_choise = int(input(">>> "))
+    if youtube_choise == 1:
+        pass
+    if youtube_choise == 2:
+        pass
+    
+user_choise = int(input(">>> "))
+quit()
---
+++ installer.bat
@@ -0,0 +1,1 @@
+pip install spotify_dl
